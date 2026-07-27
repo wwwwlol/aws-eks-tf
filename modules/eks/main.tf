619115bcbc1f554a63a2936ed9884f3f7e046522
Shalom Jacob <162062038+wwwwlol@users.noreply.github.com>
2026-07-27 19:32:43 -0400

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -4,7 +4,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.35"
 
   vpc_id                   = var.vpc_id
   subnet_ids               = var.private_subnet_ids
